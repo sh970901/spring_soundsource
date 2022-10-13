c50523d7b996531883933e68da5aa3f2b5beed28
sh970901 <aaddss639@naver.com>
2022-10-13 10:38:10 +0900

    주문 상세페이지 구현

---
+++ src/test/java/com/example/spring_music/controller/OrderControllerTests.java
@@ -0,0 +1,51 @@
+package com.example.spring_music.controller;
+
+import com.example.spring_music.app.order.controller.OrderController;
+import com.example.spring_music.app.song.service.SongService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+public class OrderControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private SongService songService;
+
+    @Test
+    @DisplayName("주문 상세화면")
+    @WithUserDetails("user2")
+    void t1() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/order/3")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(OrderController.class))
+                .andExpect(handler().methodName("showDetail"))
+                .andExpect(content().string(containsString("주문")));
+    }
+}
