d8b998446a1c7c260eee2d1ec7fd4732435eff67
sh970901 <aaddss639@naver.com>
2022-10-14 13:10:21 +0900

    정산데이터 프론트 작업

--- src/main/java/com/example/spring_music/app/member/controller/AdmHomeController.java
+++ src/main/java/com/example/spring_music/app/member/controller/AdmHomeController.java
@@ -1,8 +1,8 @@
 package com.example.spring_music.app.member.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/adm")
 public class AdmHomeController {
     @GetMapping("")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String showIndex() {
         return "redirect:/adm/home/main";
     }
 
     @GetMapping("/home/main")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String showMain() {
         return "adm/home/main";
     }
