ccebb29b99339e595d10d39306f5d220fc64f924
sh970901 <aaddss639@naver.com>
2022-10-14 16:46:52 +0900

    음원 리스트

--- src/main/java/com/example/spring_music/app/song/repositroy/SongRepository.java
+++ src/main/java/com/example/spring_music/app/song/repositroy/SongRepository.java
@@ -3,5 +3,9 @@ package com.example.spring_music.app.song.repositroy;
 import com.example.spring_music.app.song.entity.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SongRepository extends JpaRepository<Song, Long> {
+    List<Song> findAllByAuthorId(Long id);
 }
+
